02045a98b58a69a0e88e0aee01a99dc87defda88
Nitesh Surya Vanapalli <niteshvanapalli@my.unt.edu>
2025-06-13 13:22:40 -0500

    Create talker.py

---
+++ my_robot_controller/talker.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class Talker(Node):
+    def __init__(self):
+        super().__init__('talker')
+        self.publisher_ = self.create_publisher(String, 'chatter', 10)
+        timer_period = 1.0  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.get_logger().info('Talker node has started.')
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello, TurtleBot!'
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: "{msg.data}"')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Talker()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
